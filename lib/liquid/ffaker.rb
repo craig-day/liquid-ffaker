24e544e1266e2fbdf90e88d407711bc5acf39480
Craig Day <cday@zendesk.com>
2018-08-22 22:37:02 -0700

    create empty gem with `bundle gem`

---
+++ lib/liquid/ffaker.rb
@@ -0,0 +1,7 @@
+require "liquid/ffaker/version"
+
+module Liquid
+  module Ffaker
+    # Your code goes here...
+  end
+end
---
+++ lib/liquid/ffaker/version.rb
@@ -0,0 +1,5 @@
+module Liquid
+  module Ffaker
+    VERSION = "0.1.0"
+  end
+end
